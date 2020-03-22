70af21722798eab80db31dce8eeb9bf480bd6bb5
ItzSpex <itzofek@gmail.com>
2020-03-22 16:56:49 +0200

    Added new ctors and  new operators to User (==, !=)

--- PokerServer/PokerDL/Models/User.cs
+++ PokerServer/PokerDL/Models/User.cs
@@ -10,11 +10,30 @@ namespace PokerDL.Models
     [DataContract]
     public class User
     {
+        public User()
+        {
+
+        }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
         [DataMember]
         public int UserId { get; set; }
         [DataMember]
         public string Username { get; set; }
         [DataMember]
         public string Password { get; set; }
+
+        public static bool operator ==(User user1, User user2)
+        {
+            return (user1.Username == user2.Username && user1.Password == user2.Password);
+        }
+        public static bool operator !=(User user1, User user2)
+        {
+            return (user1.Username != user2.Username || user1.Password != user2.Password);
+        }
     }
 }
