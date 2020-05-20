0264135d5dd8a54a208d456b6e6593adf5a9a251
ItzSpex <itzofek@gmail.com>
2020-05-20 11:20:55 +0300

    Made the service statefull

--- PokerServer/PokerService2/IPokerService.cs
+++ PokerServer/PokerService2/IPokerService.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace PokerService
 {
-    [ServiceContract]
+    [ServiceContract(SessionMode=SessionMode.Required)]
     public interface IPokerService
     {
         [OperationContract]
